0e15e2c0086d755dadaf346698b9be4842095ce1
rseRafael <rafaelsantos.rse@gmail.com>
2018-08-26 21:49:38 -0300

    ...

---
+++ App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {createStackNavigator, createBottomTabNavigator} from 'react-navigation';
+import { VictoryChart, VictoryLine} from 'victory-line';
+import { Calendar} from 'reac-native-calendars';
+import {GymCalendar} from './Components/GymCalendar';
+
+
+class Home extends Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <View style={{ flex: 1, }}>
+        <Text>{`Hello, user.\nHere you are at our app.\nhahaha`}</Text>
+        <View>
+          <Text>{``}</Text>
+        </View>
+        
+      </View>
+    )
+  }
+}
+
+const RootStack = createBottomTabNavigator({
+  Home: {
+    screen: Home
+  },
+  Calendar: {
+    screen: GymCalendar
+  },
+  
+})
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <RootStack/>
+    );
+  }
+}
---
+++ Components/GymCalendar.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Calendar} from 'reac-native-calendars';
+
+
+class GymCalendar extends Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <View>
+                <Calendar
+                    markedDates={{
+                        '2018-08-09':{selected: true, selectedColor: "blue"}, 
+                        '2018-08-19': {selected: true, selectedColor: "green"}
+                    }}
+                />
+            </View>
+        )
+    }
+}
